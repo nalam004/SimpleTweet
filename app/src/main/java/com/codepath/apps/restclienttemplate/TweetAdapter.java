33fb7eec65d08487aa2e3108a823dbf34cdad14f
nalam004 <niharikaalam1234@gmail.com>
2021-10-01 22:48:32 -0400

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TweetAdapter.java
@@ -0,0 +1,77 @@
+package com.codepath.apps.restclienttemplate;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.codepath.apps.restclienttemplate.models.Tweet;
+
+import java.util.List;
+
+public class TweetAdapter extends RecyclerView.Adapter<TweetAdapter.ViewHolder> {
+
+    Context context;
+    List<Tweet> tweets;
+
+    public TweetAdapter (Context context, List<Tweet> tweets) {
+        this.context = context;
+        this.tweets = tweets;
+    }
+
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.item_tweet, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        Tweet tweet = tweets.get(position);
+        holder.bind(tweet);
+    }
+
+    public int getItemCount() {
+        return tweets.size();
+    }
+
+    public void clear() {
+        tweets.clear();
+        notifyDataSetChanged();
+    }
+
+    public void addAll(List<Tweet> tweetList) {
+        tweets.addAll(tweetList);
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView profileImg;
+        TextView tvBody;
+        TextView name;
+        TextView screenName;
+        TextView timeStamp;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            profileImg = itemView.findViewById(R.id.profileImg);
+            tvBody = itemView.findViewById(R.id.tvBody);
+            name = itemView.findViewById(R.id.name);
+            screenName = itemView.findViewById(R.id.screenName);
+            timeStamp = itemView.findViewById(R.id.timeStamp);
+        }
+
+        public void bind(Tweet tweet) {
+            tvBody.setText(tweet.body);
+            timeStamp.setText(tweet.timeStamp);
+            name.setText(tweet.user.name);
+            screenName.setText("@" + tweet.user.screenName);
+            Glide.with(context).load(tweet.user.profileImgURL).into(profileImg);
+        }
+    }
+}
---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -0,0 +1,66 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import android.text.format.DateUtils;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class Tweet {
+    public String body;
+    public String createdAt;
+    public String timeStamp;
+    public long id;
+    public User user;
+
+    public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
+        Tweet tweet = new Tweet();
+        tweet.body = jsonObject.getString("text");
+        tweet.createdAt = getDate(jsonObject.getString("created_at"));
+        tweet.timeStamp = getTimestamp(jsonObject.getString("created_at"));
+        tweet.id = jsonObject.getLong("id");
+        tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+        return tweet;
+    }
+
+    public static List<Tweet> fromJsonArray(JSONArray jsonArray) throws JSONException {
+        List<Tweet> tweets = new ArrayList<>();
+        for(int i = 0; i < jsonArray.length(); i++) {
+            tweets.add(fromJson(jsonArray.getJSONObject(i)));
+        }
+        return tweets;
+    }
+
+    public static String getTimestamp(String rawJsonDate) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.setLenient(true);
+        String relativeDate = "";
+        try {
+            long dateMillis = sf.parse(rawJsonDate).getTime();
+            relativeDate = DateUtils.getRelativeTimeSpanString(dateMillis,
+                    System.currentTimeMillis(), DateUtils.SECOND_IN_MILLIS, DateUtils.FORMAT_ABBREV_RELATIVE ).toString();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return relativeDate;
+    }
+
+    public static String getDate(String rawJsonDate) {
+        String twitterFormat = "EEE MMM dd HH:mm:ss ZZZZZ yyyy";
+        String newFormat = "h:mm, MMM dd, yyyy";
+        SimpleDateFormat sf = new SimpleDateFormat(twitterFormat, Locale.ENGLISH);
+        sf.applyPattern(newFormat);
+        String newDate = sf.format(new Date(rawJsonDate));
+        return newDate;
+    }
+
+
+}
